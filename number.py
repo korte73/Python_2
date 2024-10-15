32bec3c065bedf56da9feb64118a463884cfb837
korte73 <korte@kortefoto.hu>
2024-10-15 10:11:52 +0200

    1.1

--- number.py
+++ number.py
@@ -3,15 +3,12 @@ def main():
     print(f"x is{x}")
 
 
-    def get_int():
-        while True:
-            try:
-                x = int(input("What's x?"))
-            except ValueError:
-                print("x is not an integer")
-            else:
-                break
-        return x
-
+def get_int():
+    while True:
+        try:
+            return int(input("What's x?"))
+        except ValueError:
+            print("x is not an integer")
+       
 
 main()
